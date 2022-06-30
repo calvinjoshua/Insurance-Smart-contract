63192c7125b48dbfa815d9f3b4ffb61c15a6a454
calvinjoshua <calvin@diamanteblockchain.com>
2022-06-30 21:12:08 +0530

    add unit testcases

--- test/index.ts
+++ test/index.ts
@@ -2,18 +2,20 @@ import { expect } from "chai";
 import { ethers } from "hardhat";
 
 describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  var Main
+  var main
+  var owner
+  var addr1
+  var addr2
+  var addr3
+  var addr4
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
-
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  beforeEach(async function () {
+  
   });
+
+  
+
+
 });
